2d63d040abce72df90d933938969e66607efdeea
chenjiahau <chenjiahau@gmail.com>
2022-12-27 15:12:19 +0800

    Test

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ const drawChart = async () => {
     .sort(null)
     .value(d => d.p);
 
-  const artPath = d3.arc()
+  const arcPath = d3.arc()
     .outerRadius(pieChart.radius.outer)
     .innerRadius(pieChart.radius.inner);
 
@@ -45,12 +45,23 @@ const drawChart = async () => {
   const paths = graph.selectAll('path')
     .data(pie(data));
 
+
+  const arcTweenEnter = (d) => {
+    var i = d3.interpolate(d.endAngle - 0.1, d.startAngle);
+
+    return function (t) {
+      d.startAngle = i(t);
+      return arcPath(d);
+    };
+  };
+
   paths.enter()
     .append('path')
-    .attr('d', artPath)
+    .attr('d', arcPath)
     .attr('stroke', pieChart.stroke.color)
     .attr('stroke-width', pieChart.stroke.width)
-    .attr('fill', d => colour(d.data.name));
+    .attr('fill', d => colour(d.data.name))
+    .transition().duration(3000).attrTween("d", arcTweenEnter);
 
   paths.enter()
     .append('text')
@@ -58,7 +69,7 @@ const drawChart = async () => {
       if (d.data.p > 0)
         return d.data.name
     })
-    .attr('transform', d => `translate(${artPath.centroid(d)})`)
+    .attr('transform', d => `translate(${arcPath.centroid(d)})`)
     .classed('label', true);
 }
 
