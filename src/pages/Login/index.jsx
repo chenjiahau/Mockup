9f55f78e67045ff095475f48764c6dae6215d987
ivanchen <ivan.chen@dlinkcorp.com>
2024-11-01 19:16:11 +0800

    Adjust login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -19,69 +19,71 @@ const Login = () => {
 
   return (
     <div className='login-container'>
-      <div className='login-block login-header light-primary-shadow'>
-        <div className='icon'>
-          <img src={logo} />
+      <div className='login-border light-primary-shadow'>
+        <div className='login-block login-header light-primary-shadow'>
+          <div className='icon'>
+            <img src={logo} />
+          </div>
+          <MainTitle extraClasses={["!mb-0"]}>
+            Management Information System
+          </MainTitle>
         </div>
-        <MainTitle extraClasses={["!mb-0"]}>
-          Management Information System
-        </MainTitle>
-      </div>
 
-      <div className='login-block login-form light-primary-shadow'>
-        <FormGroup>
-          <FormLabel forName='email'>E-mail</FormLabel>
-          <InputBox
-            type='text'
-            id='email'
-            name='email'
-            placeholder='E-mail'
-            value={email}
-            onChange={(value) => setEmail(value)}
-          />
-        </FormGroup>
+        <div className='login-block login-form light-primary-shadow'>
+          <FormGroup>
+            <FormLabel forName='email'>E-mail</FormLabel>
+            <InputBox
+              type='text'
+              id='email'
+              name='email'
+              placeholder='E-mail'
+              value={email}
+              onChange={(value) => setEmail(value)}
+            />
+          </FormGroup>
 
-        <FormGroup>
-          <FormLabel forName='password'>Name</FormLabel>
-          <InputBox
-            type={isPasswordVisible ? "text" : "password"}
-            id='password'
-            name='password'
-            placeholder='Password'
-            value={password}
-            onChange={(value) => setPassword(value)}
+          <FormGroup>
+            <FormLabel forName='password'>Name</FormLabel>
+            <InputBox
+              type={isPasswordVisible ? "text" : "password"}
+              id='password'
+              name='password'
+              placeholder='Password'
+              value={password}
+              onChange={(value) => setPassword(value)}
+            >
+              {
+                <FontAwesomeIcon
+                  icon={isPasswordVisible ? faEyeSlash : faEye}
+                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                />
+              }
+            </InputBox>
+          </FormGroup>
+        </div>
+
+        <div className='login-block login-button'>
+          <ButtonBox onClick={() => {}} isSave={true}>
+            Save
+          </ButtonBox>
+          <ButtonBox
+            onClick={() => {}}
+            isClose={true}
+            extraClasses={["cancel-shadow"]}
           >
-            {
-              <FontAwesomeIcon
-                icon={isPasswordVisible ? faEyeSlash : faEye}
-                onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-              />
-            }
-          </InputBox>
-        </FormGroup>
-      </div>
+            Close
+          </ButtonBox>
+        </div>
 
-      <div className='login-block login-button'>
-        <ButtonBox onClick={() => {}} isSave={true}>
-          Save
-        </ButtonBox>
-        <ButtonBox
-          onClick={() => {}}
-          isClose={true}
-          extraClasses={["cancel-shadow"]}
-        >
-          Close
-        </ButtonBox>
-      </div>
+        <div className='login-block login-link'>
+          <LinkButton to='/forgot-password' title='Forget Password' />
+          <div>|</div>
+          <LinkButton to='/signup' title='Sign Up' />
+        </div>
 
-      <div className='login-block login-link'>
-        <LinkButton to='/forgot-password' title='Forget Password' />
-        <div>|</div>
-        <LinkButton to='/signup' title='Sign Up' />
-      </div>
-
-      <div className='login-block login-footer light-primary-shadow'>
-        ©ISP-START
+        <div className='login-block login-footer light-primary-shadow'>
+          © 2024 Ivan Solutions. All rights reserved.
+        </div>
       </div>
     </div>
   );
