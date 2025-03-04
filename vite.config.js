b319e1d719c0ee6987c2906dea245c995cdf7079
chenjiahau <ivan.chen@dlinkcorp.com>
2025-03-04 15:32:07 +0800

    Initializing project structure 1. Removing useless files(index.css). 2. Emptying App.css. 3. Setting alias path(@ refer to the src directory).

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Set '@' to refer to the 'src' directory
+    },
+  },
 })
